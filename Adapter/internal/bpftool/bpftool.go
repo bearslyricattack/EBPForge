98f3f007ebeb6aef6e077226e3c5db1a86d0fbf7
抒情熊 <2669184984@qq.com>
2025-05-11 14:31:36 +0800

    重构controller模块

--- Adapter/internal/bpftool/bpftool.go
+++ Adapter/internal/bpftool/bpftool.go
@@ -9,6 +9,8 @@ import (
 func ReadMapUsingTool(path string) (string, error) {
 	cmd := exec.Command("bpftool", "map", "dump", "pinned", path)
 	output, err := cmd.CombinedOutput()
+	fmt.Println("获取结果")
+	fmt.Println(string(output))
 	if err != nil {
 		return "", fmt.Errorf("failed to execute bpftool: %v, output: %s", err, output)
 	}
